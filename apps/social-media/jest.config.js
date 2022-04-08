07fd4e99a3fc4b880830485d893679043481a50b
CheesStick <ismenotyousogood@gmail.com>
2022-04-08 17:21:41 +0100

    initial commit

---
+++ apps/social-media/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'social-media',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/social-media',
+};
